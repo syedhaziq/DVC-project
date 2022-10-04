c2467eb82b0c8ea05a09c6f4a86185ae6ee26cc9
Syed Muhammad Kamran <syed_mkamran@outlook.com>
2022-10-05 00:30:31 +0500

    some progress

---
+++ README.md
@@ -0,0 +1,30 @@
+create env
+
+```bash
+conda create -n wineq python=3.7 -y
+```
+
+active
+
+```bash
+conda activate wineq 
+```
+
+created a requirements.txt file
+
+```bash
+touch requirements.txt
+```
+
+Installed the required dependencies 
+``bash
+pip install -r requirements.txt
+```
+
+git init
+
+dvc init
+
+dvc add data_given/winequality.csv
+
+
---
+++ template.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  4 23:51:19 2022
+
+@author: syed_
+"""
+
+import os
+
+
+dirs = [
+       os.path.join("data","raw"),
+        os.path.join("data","processed"),
+        "notebooks",
+        "save_models",
+        "src"
+        
+        ]
+
+for dir_ in dirs:
+    os.makedirs(dir_, exist_ok= True)
+    print(dir_)
+    
+    with open(os.path.join(dir_,".gitkeep"),"w") as f:
+        pass  
+        
+    
+
+file_ = [
+    "dvc.yaml",
+    "params.yaml",
+    ".gitignore",
+    os.path.join("src","__init__.py")
+
+    ]
+
+
+for files in file_:
+    with open(files, "w") as f:
+        pass
