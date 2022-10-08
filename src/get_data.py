cb4f1d3595c72f301bc0db643c38938d97eb4bcc
Syed Muhammad Kamran <syed_mkamran@outlook.com>
2022-10-09 01:02:45 +0500

    get_data file completed

---
+++ src/get_data.py
@@ -0,0 +1,34 @@
+## Read the param
+## process
+## return the dataframe
+
+
+import os
+import yaml
+import pandas as pd
+import argparse
+
+def read_data(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def get_data(config_path):
+    #print(config_path)
+    config = read_data(config_path)
+    #print(config)
+    data_path = config["data_source"]["s3_source"]
+    data = pd.read_csv(data_path, sep=".")
+    print(data.head())
+    
+    
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default = "params.yaml")
+    parsed_args = args.parse_args()
+    get_data(config_path= parsed_args.config)
+    
+
+    
